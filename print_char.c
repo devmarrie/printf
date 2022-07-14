f2392d30a08b0b1ede63b7c8a378cc16d2b536c6
root <annemarrieakeyo42@gmail.com>
2022-07-14 15:13:20 -0700

    [feat]:the char function

---
+++ print_char.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*print_char returns the character
+*@buff_dest: buffer
+*@buff_count: index of buffer pointer
+*Return: buffer index
+*/
+
+int print_char(char *buff_dest, va_list arg, int buff_count)
+{
+       char c = va_arg(arg, int);
+
+
+	buff_dest[buff_count] = c;
+
+	return (++buff_count);
+
+}
+
